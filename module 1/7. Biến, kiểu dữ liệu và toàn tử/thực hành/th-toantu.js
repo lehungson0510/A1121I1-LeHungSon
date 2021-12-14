3c55cf6b27ef98504eba984ba09bd09941f7d91c
lehungson0510 <hungsonle0510@gmail.com>
2021-12-14 17:34:12 +0700

    thuc hanh

---
+++ module 1/7. Biến, kiểu dữ liệu và toàn tử/thực hành/th-toantu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth= prompt("Enter the Width");
+inputHeight= prompt("Enter the Height");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area= width*height;
+document.write("The area is: "+area);
